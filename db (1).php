9348fd5ff83d7c1a8b496c9356d51ea008d58678
domszaa <daraneta51@gmail.com>
2025-09-04 14:59:52 +0800

    Add files via upload

---
+++ db (1).php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$db   = 'atiera_1';
+$user = 'root';    
+$pass = '';        
+
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("DB Connection failed: " . $e->getMessage());
+}
+?>
